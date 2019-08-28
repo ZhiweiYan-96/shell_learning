97ff860e40e16dd254d913753b40fcb9ed551ecc
14zwyan <532707544@qq.com>
2019-08-28 09:01:45 +0800

    update variable.sh

--- variable.sh
+++ variable.sh
@@ -13,7 +13,21 @@ for file in `ls /etc`; do
     echo $file
 done
 
-#{} can bse use to explicitly show the boundar of variable 
+#{} can bse use to explicitly show the boundary of variable 
 for skill in Ada Coffee Action Jave; do 
     echo "I am good at ${skill}Script"
 done 
+
+#readonly variable 
+myUrl="https://github.com"
+readonly myUrl
+#follwing line will complain since the readonly variable cann't be changed
+#myUrl="https://goole.com"
+
+#Delte variable 
+#unset myUrl  #Read Only Variable cannot be deleted 
+#echo $myUrl  
+unset hello 
+#Since hello is deleted, there will be no output 
+echo $hello 
+
