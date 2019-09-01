6dcc2f4200ddaabffe54fd8ca5f7993f30ffa706
14zwyan <532707544@qq.com>
2019-09-01 20:29:23 +0800

    update string part

--- variable.sh
+++ variable.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+!/bin/bash 
 
 #define variable 
 # No space between variable name equation symobl 
@@ -40,3 +40,24 @@ your_name='hello'
 str="Hello, I know your are \"$your_name\"!\n"
 echo -e $str
 
+#concat string 
+your_name="runoob"
+greeting="hello."$your_name"!"
+greeting_1="hello,${your_name}!"
+echo $greeting $greeting_1 
+#concat using single quote 
+greeting_2='hello,'$your_name'!'
+greeting_3='hello,${your_name}!'
+echo $greeting_2 $greeting_3 
+
+#get length of string 
+string='abcd'
+echo ${#string}
+
+#extract substring 
+string="runnoob is a great site"
+echo ${string:1:4}
+
+#find substring, output the location of finded substring
+string="runnob is a great site"
+echo `expr index "$string" io`
