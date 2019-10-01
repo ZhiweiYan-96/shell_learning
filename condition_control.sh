dad091c392a3eb5376092b5299baa0743993129d
14zwyan <532707544@qq.com>
2019-10-02 00:10:28 +0800

    Add continue,break statement

--- condition_control.sh
+++ condition_control.sh
@@ -78,4 +78,68 @@ do
 	let "int++"
 done 
 
+#case statement 
+#case value in 
+# patter1)
+#	command1
+#	command2
+#	;;
+# pattern2)
+#	command1
+#	;;
+#esac 
 
+echo "Please input value in [1,4]:"
+read aNum 
+case $aNum in 
+	1) 
+	echo "You choose 1"
+	;;
+	2)
+	echo "You choose 2"
+	;;
+	3)
+	echo "You chosse 3"
+	;;
+	4)
+	echo "You choose 4"
+	;;
+	*)
+	echo "Your number is not in [1,4]"
+	;;
+esac 
+
+
+#break 
+while : 
+do 
+	echo -n "Please input a number in [1,5]"
+	read aNum 
+	case $aNum in 
+	1|2|3|4|5)
+	echo "The number you input is $aNum"
+	;;
+	*)
+	echo "The number you input is not in [1,5], Jump out of loop"
+	break 
+	;;
+	esac 
+done 
+
+#continue 
+while : 
+do 
+	echo -n "Please input a number in [1,5]"
+	read aNum 
+	case $aNum in 
+		1|2|3|4|5) 
+		echo "Thu number you input is $aNum"
+		;;
+		*)
+		echo "The number you input is not in [1,5]"
+		continue 
+		#This code is a permanet loop"
+		echo "Game over"
+		;;
+	esac 
+done 
